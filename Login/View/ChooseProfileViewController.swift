48fb3bb210af0a743c757cd6c17b0acd94c8e162
sagarchavan2001 <chavansagar6944@gmail.com>
2024-02-28 20:17:48 +0530

    Job portal

---
+++ Login/View/ChooseProfileViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ChooseProfileViewController.swift
+//  JobPortalApp
+//
+//  Created by STC on 22/02/24.
+//
+
+import UIKit
+
+class ChooseProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recruiterBtin(_ sender: Any) {
+        
+        let nav = self.storyboard?.instantiateViewController(withIdentifier: "RecruiterProfileViewController")as! RecruiterProfileViewController
+        self.navigationController?.pushViewController(nav, animated: true)
+    }
+    
+    
+    @IBAction func studentBtn(_ sender: Any) {
+        let nav = self.storyboard?.instantiateViewController(withIdentifier: "StudentProfileViewController")as! StudentProfileViewController
+        self.navigationController?.pushViewController(nav, animated: true)
+    }
+    
+
+}
---
+++ Student/StudentProfileViewController.swift
@@ -0,0 +1,55 @@
+//
+//  StudentProfileViewController.swift
+//  JobPortalApp
+//
+//  Created by STC on 26/02/24.
+//
+
+import UIKit
+
+class StudentProfileViewController: UIViewController {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
